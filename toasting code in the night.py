8da35688bf222c7359e41e7ec9545adb6590b127
marviee <37642775+marviee@users.noreply.github.com>
2019-02-25 00:39:17 +0100

    Add files via upload

---
+++ toasting code in the night.py
@@ -0,0 +1,154 @@
+gal= input("what is your name dearie: ")
+h=input(gal + ", do u love me (Y/N): ")
+if h=="Y":
+    q=input(gal+", reallyyyyyyyyyy???? [Y/N]:  ")
+    if q=="Y": 
+        print("Marvie loves you more")
+        input("can you do me a favor [Y/N] :")
+        input("promise you will do it:  [Y/N]")
+        p = input("promise first, plsss..... [okay/never]")
+        if p == "okay":
+            input("hmm.. thanks lovee, i wish to kiss you...plss")
+            input("plsssssss, look me in the eye..")
+            input("what now... life is not this hard o..")
+            input("hmmm... Have you kissed before?? [Y/N]: gist me    ")
+            input("Then, pls consider me na, plsss, just this once...: [Y/N]")
+            input("plss...: ")
+        
+        
+            input("okayy, at least peck na... [Y/N]:  ")
+            input("okay ooo")
+        else:
+            input("hmm.., i wish to kiss you...plss")
+            input("plsssssss, look me in the eye..")
+            input("what now... life is not this hard o..")
+            input("hmmm... Have you kissed before?? [Y/N]: ")
+            input("Then, pls consider me na, plsss, just this once...: [Y/N]")
+            input("plss...: ")
+        
+            input("okayy, at least peck na... [Y/N]:  ")
+            input("okay ooo")
+        
+    else:
+        input("ahnahn, well, marvie loves you still  ")
+        input("can you do me a favor [Y/N] : ")
+        input("promise you will do it:  [Y/N]  ")
+        p = input("promise first, plsss..... [okay/never]")
+        if p == "okay":
+            input("hmm.. thanks lovee, i wish to kiss you...plss")
+            input("plsssssss, look me in the eye..")
+            print("what now... life is not this hard o..")
+            input("hmmm... Have you kissed before?? [Y/N]: gist me    ")
+            input("Then, pls consider me na, plsss, just this once...: [Y/N]")
+            input("plss...: ")
+        
+        
+            input("okayy, at least peck na... [Y/N]:  ")
+            input("okay ooo")
+        else:
+            input("hmm.., i wish to kiss you...plss")
+            input("plsssssss, look me in the eye..")
+            input("what now... life is not this hard o..")
+            input("hmmm... Have you kissed before?? [Y/N]: ")
+            input("Then, pls consider me na, plsss, just this once...: [Y/N]")
+            input("plss...: ")
+        
+            input("okayy, peck plss... [Y/N]:  ")
+            input("okay ooo:")
+            input("pls dont go tomorrow..")
+            input("love energy..")
+            
+    
+        
+    
+else:
+    love = 10
+    while(love<14):
+        print("seriously, " + gal + ", Marvie really loves you...")
+        love+=1
+    r= input("Do you believe??....[Y/N] ")
+    if r == "Y":
+        input(gal + ", can you ever love me? [Y/N]: ")
+        
+        print("okay sha...")
+        input(gal + ", pls na... promise if you stay, me, you know, I'm gentle and open, i won't disapoint...plss: [Y/N}")
+
+        input("sayy something plssssssssssssssssss......................  :")
+        input('Marvie loves you still, and i believe you will change your mind soonest right?  [Y/N]')
+        input("hmmmm... ")
+        input("go jo")
+        input("well, marvie loves you still")
+        input("can you do me a favor [Y/N] :")
+        input("promise you will do it:  [Y/N]")
+        p = input("promise first, plsss..... [okay/never] ")
+        if p == "okay":
+            print("hmm.. thanks lovee, i wish to kiss you...plss")
+            print("plsssssss, look me in the eye..")
+            print("what now... life is not this hard o..")
+            input("hmmm... Have you kissed before?? [Y/N]: gist me    ")
+            input("Then, pls consider me na, plsss, just this once...: [Y/N]")
+            input("plss...: ")
+        else:
+            print("hmm.., i wish to kiss you...plss")
+            print("plsssssss, look me in the eye..")
+            print("what now... life is not this hard o..")
+            input("hmmm... Have you kissed before?? [Y/N]: ")
+            input("Then, pls consider me na, plsss, just this once...: [Y/N]")
+            input("plss...: ")
+        
+            input("okayy, at least peck na... [Y/N]:  ")
+            input("okay ooo")
+            
+        
+    else:
+        input("so you dont know... look na...it's all written all over my face...I'm saying with all my heart that i really love you..:  ")
+        print("hmmm...")
+        input(gal + ", can you ever love me? [Y/N]: ")
+        
+        print("okay sha...")
+        input(gal + "pls na... promise if you stay, me, you know, I'm gentle and open, i won't disapoint...plss: [Y/N}")
+
+        input("sayy something plss......................  :")
+        print('Marvie loves you still, and i believe you will change your mind soonest right?  [Y/N]')
+        print("hmmmm... ")
+        print("go jo")
+        print("well, marvie loves you still")
+        input("can you do me a favor [Y/N] :")
+        input("promise you will do it:  [Y/N]")
+        p = input("promise first, plsss..... [okay/never]")
+        if p == "okay":
+            print("hmm.. thanks lovee, i wish to kiss you...plss")
+            print("plsssssss, look me in the eye..")
+            print("what now... life is not this hard o..")
+            input("hmmm... Have you kissed before?? [Y/N]: gist me    ")
+            input("Then, pls consider me na, plsss, just this once...: [Y/N]")
+            input("plss...: ")
+            input("pls dont go tomorrow..")
+            input("love energy..")
+      
+        else:
+            input(gal+ " ahnahn... ")
+            input("hmmm.., the thing is, i wish to kiss you...plss  ")
+            input("plsssssss, look me in the eye..")
+            input("hmm.. life shaa.. ")
+            print(gal+ ", pls talk...")
+            input("hmmm... Have you kissed before?? [Y/N]: ")
+            input("Then, pls consider me na, plsss, just this once...: [Y/N]")
+            input("plss...: ")
+        
+            input("okayy, at least peck na... [Y/N]:  ")
+            input("hmm... okay ooo")
+            input("pls dont go tomorrow..")
+            input("love energy..")
+
+            for i in range(6):
+                input(gal+", plssssssss, love me pls")
+                for j in range (i):
+                    input(gal+", plssssssss ")
+                
+                    
+print("love energy")
+    
+   
+
+
